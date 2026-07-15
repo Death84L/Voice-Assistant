a0e0016a1c05089410b688e06758c3926f353226
Kausstubh Jaiswal <kausstubhjaiswal91256@gmail.com>
2026-07-15 12:31:11 +0530

    Add stream route

--- cmd/main.go
+++ cmd/main.go
@@ -40,6 +40,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/translate", handler.TranslateAudio)
+	mux.HandleFunc("/translate/stream", handler.TranslateStream)
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("ok"))
